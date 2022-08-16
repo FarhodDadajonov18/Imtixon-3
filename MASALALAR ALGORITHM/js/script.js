969963508ab6aa1d0829848f32645f24cde7edec
dadajonovfarhod <dadajonovfarhod5@gmail.com>
2022-08-16 05:58:58 +0500

    added new file html

---
+++ MASALALAR ALGORITHM/js/script.js
@@ -0,0 +1,56 @@
+
+// // 1 - Masala 
+// reverseName("Alisher Navoiy");
+
+// function reverseName(text){
+//   let array = text.split(" ").reverse().join(" ");  
+//   return console.log(array);
+// }
+
+
+
+
+
+
+
+// // 2 -Masala 
+
+// squareNumber([2,4,9,3,5]);
+
+// function squareNumber(numbers) {
+//     let arr = [];
+
+//     for(let element of numbers) {
+//         if (Math.sqrt(element) % 1 === 0) {
+//             arr.push(Math.sqrt(element));
+//         }
+//         else {
+//             arr.push(element*element);
+//         }
+//     }
+//     return console.log(arr );
+// }
+
+
+
+
+
+
+
+
+
+//   // 3-Masala
+// var a = "Hello World";
+
+// function example(a){
+
+//   var result = "";
+//   for(var i=0; i < a.length; i++){
+//   result += a[i]+a[i];
+//   }
+//   return result;
+  
+// }
+
+// console.log(example(a));
+
